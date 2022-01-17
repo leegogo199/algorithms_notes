c6b258ed88fc60864784ddffbabd10ffcbfa2aae
leegogo199 <mianfeizuce12@163.com>
2022-01-17 09:38:46 +0800

    Chapter2

---
+++ Chapter2.go
@@ -0,0 +1,37 @@
+package algor_notes
+//算法分析
+/*
+观察
+数学模型
+分类
+算法理论
+记忆
+每个角色 不同
+程序员解决
+客户 高效解决
+理论 为啥work
+避免性能错误。
+举例 快速傅里叶变换 算法。
+我的程序在大输入的时候，还能正常工作吗？
+slow，run out of memory。
+观察，假设，预测，验证，矫正。
+原则：
+实验可重复
+假设可证伪。
+
+ */
+// 观察
+/*
+just test and get answer
+估计算法的运行时间。
+对性能做出预测，但不能帮助我们理解算法做了什么。
+
+ */
+//数学模型
+/*
+total running time
+sum of cost*frequency for all operations
+need to analyze program to determine set of operations
+cost depends on machine,compiler
+frequency depends on algorithm,input data.
+ */
